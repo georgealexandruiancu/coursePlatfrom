646344d391a1150b1732f5f1a435668fbb70cf8a
georgealexnadruiancu <alex1gtp@gmail.com>
2018-11-08 09:39:02 +0200

    copyright added

--- js/ui.js
+++ js/ui.js
@@ -1,4 +1,31 @@
-var w = window.innerWidth;
-if(w < 800){
+$(document).ready(function () {
+
+    // hide our element on page load
+    $('#titleSplashHolder').css('opacity', 0);
+    $('#btnSplashHolder').css('opacity', 0); 
+    $('#left-featured').css('opacity', 0);
+    $('#center-featured').css('opacity', 0);
+    $('#right-featured').css('opacity', 0);
+    $('#leftInfo').css('opacity', 0);
+    $('#rightInfo').css('opacity', 0);
+
+
+
+    $('.splashHolder').waypoint(function () {
+        $('#titleSplashHolder').addClass('fadeInLeft');
+        $('#btnSplashHolder').addClass('fadeInUpBig');
+    }, { offset: '50%' });
+
     
-}
+    $('#featuresPannel').waypoint(function () {
+        $('#left-featured').addClass('fadeInLeft');
+        $('#center-featured').addClass('fadeInUpBig');
+        $('#right-featured').addClass('fadeInRight');
+    }, { offset: '70%' });
+
+    $('#courseDescription').waypoint(function () {
+        $('#leftInfo').addClass('fadeInLeft');
+        $('#rightInfo').addClass('fadeInRight');
+    }, { offset: '70%' });
+
+});
