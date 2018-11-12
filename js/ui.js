908b8b3dc67985747c80f64c89109f7c99ca024b
georgealexnadruiancu <alex1gtp@gmail.com>
2018-11-12 17:20:52 +0200

    yes

--- js/ui.js
+++ js/ui.js
@@ -8,6 +8,9 @@ $(document).ready(function () {
     $('#right-featured').css('opacity', 0);
     $('#leftInfo').css('opacity', 0);
     $('#rightInfo').css('opacity', 0);
+    $('#holder1Reg').css('opacity', 0);
+    $('#holder2Reg').css('opacity', 0);
+
 
 
 
@@ -27,5 +30,33 @@ $(document).ready(function () {
         $('#leftInfo').addClass('fadeInLeft');
         $('#rightInfo').addClass('fadeInRight');
     }, { offset: '70%' });
+
+    $('#courseDescription').waypoint(function () {
+        $('#leftInfo').addClass('fadeInLeft');
+        $('#rightInfo').addClass('fadeInRight');
+    }, { offset: '70%' });
+
+    $('#animHolder1Reg').waypoint(function () {
+        $('#holder1Reg').addClass('fadeInLeft');
+    }, { offset: '10%' });
+
+    $('#animHolder2Reg').waypoint(function () {
+        $('#holder2Reg').addClass('fadeInLeft');
+    }, { offset: '10%' });
+    $('#animFadeOutReg').waypoint(function (direction) {
+        if (direction === 'down') {
+            $('#holder1Reg').addClass('fadeOutLeft');
+            $('#holder2Reg').addClass('fadeOutLeft');
+            $('#holder1Reg').removeClass('fadeInLeft');
+            $('#holder2Reg').removeClass('fadeInLeft');
+        }
+        else if (direction === 'up'){
+            
+            $('#holder1Reg').addClass('fadeInLeft');
+            $('#holder2Reg').addClass('fadeInLeft');
+            $('#holder1Reg').removeClass('fadeOutLeft');
+            $('#holder2Reg').removeClass('fadeOutLeft');
+        }   
+    }, { offset: '100px' });
 
 });
